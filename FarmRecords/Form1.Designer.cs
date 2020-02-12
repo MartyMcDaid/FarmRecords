147e148fbaa4dbb2b79159e8aec950880060c5b0
marty.mcdaid@gmail.com <marty.mcdaid@gmail.com>
2020-02-12 15:51:58 +0000

    Renamed main form and added a simple Menu Strip.

--- FarmRecords/Form1.Designer.cs
+++ FarmRecords/Form1.Designer.cs
@@ -28,13 +28,76 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eXitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sheepToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addNewSheepToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.sheepToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.eXitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // eXitToolStripMenuItem
+            // 
+            this.eXitToolStripMenuItem.Name = "eXitToolStripMenuItem";
+            this.eXitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.eXitToolStripMenuItem.Text = "E&Xit";
+            // 
+            // sheepToolStripMenuItem
+            // 
+            this.sheepToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addNewSheepToolStripMenuItem});
+            this.sheepToolStripMenuItem.Name = "sheepToolStripMenuItem";
+            this.sheepToolStripMenuItem.Size = new System.Drawing.Size(51, 20);
+            this.sheepToolStripMenuItem.Text = "&Sheep";
+            // 
+            // addNewSheepToolStripMenuItem
+            // 
+            this.addNewSheepToolStripMenuItem.Name = "addNewSheepToolStripMenuItem";
+            this.addNewSheepToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.addNewSheepToolStripMenuItem.Text = "&Add New Sheep";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "FarmRecords";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem eXitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sheepToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addNewSheepToolStripMenuItem;
     }
 }
 
