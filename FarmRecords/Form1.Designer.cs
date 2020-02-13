d41e0dbfb6bac91f038855e3609bfbf48f44cef1
marty.mcdaid@gmail.com <marty.mcdaid@gmail.com>
2020-02-13 10:18:27 +0000

    Added Photo to Main page and added Sheep button to main page

--- FarmRecords/Form1.Designer.cs
+++ FarmRecords/Form1.Designer.cs
@@ -34,14 +34,14 @@
             this.sheepToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.addNewSheepToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.mainExitButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.mainSheep = new System.Windows.Forms.Button();
             this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // menuStrip1
             // 
-            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.fileToolStripMenuItem,
-            this.sheepToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
             this.menuStrip1.Size = new System.Drawing.Size(800, 24);
@@ -59,7 +59,7 @@
             // eXitToolStripMenuItem
             // 
             this.eXitToolStripMenuItem.Name = "eXitToolStripMenuItem";
-            this.eXitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.eXitToolStripMenuItem.Size = new System.Drawing.Size(93, 22);
             this.eXitToolStripMenuItem.Text = "E&xit";
             this.eXitToolStripMenuItem.Click += new System.EventHandler(this.eXitToolStripMenuItem_Click);
             // 
@@ -87,11 +87,33 @@
             this.mainExitButton.UseVisualStyleBackColor = true;
             this.mainExitButton.Click += new System.EventHandler(this.mainExitButton_Click);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox1.Image = global::FarmRecords.Properties.Resources.DSC_0745;
+            this.pictureBox1.Location = new System.Drawing.Point(141, 63);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(559, 314);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // mainSheep
+            // 
+            this.mainSheep.Location = new System.Drawing.Point(37, 63);
+            this.mainSheep.Name = "mainSheep";
+            this.mainSheep.Size = new System.Drawing.Size(75, 23);
+            this.mainSheep.TabIndex = 3;
+            this.mainSheep.Text = "&Sheep";
+            this.mainSheep.UseVisualStyleBackColor = true;
+            // 
             // MainMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.mainSheep);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.mainExitButton);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
@@ -99,6 +121,7 @@
             this.Text = "FarmRecords";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -112,6 +135,8 @@
         private System.Windows.Forms.ToolStripMenuItem sheepToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem addNewSheepToolStripMenuItem;
         private System.Windows.Forms.Button mainExitButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button mainSheep;
     }
 }
 
