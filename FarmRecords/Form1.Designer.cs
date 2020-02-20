cacee82760e08ce37e816371de138ab95e908352
marty.mcdaid@gmail.com <marty.mcdaid@gmail.com>
2020-02-20 20:13:00 +0000

    Changed anchors for pictures on Main Form and Sheep Form

--- FarmRecords/Form1.Designer.cs
+++ FarmRecords/Form1.Designer.cs
@@ -145,7 +145,9 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.pictureBox1.Image = global::FarmRecords.Properties.Resources.DSC_0745;
             this.pictureBox1.Location = new System.Drawing.Point(141, 92);
--- FarmRecords/SheepForm.Designer.cs
+++ FarmRecords/SheepForm.Designer.cs
@@ -146,7 +146,9 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.pictureBox1.Image = global::FarmRecords.Properties.Resources.DSC_0740;
             this.pictureBox1.Location = new System.Drawing.Point(141, 92);
             this.pictureBox1.Name = "pictureBox1";
