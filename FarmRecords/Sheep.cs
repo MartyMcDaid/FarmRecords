e8b101cf63cb1473d5cf7b7c2381fd0d6933d855
marty.mcdaid@gmail.com <marty.mcdaid@gmail.com>
2020-06-22 21:57:47 +0100

    Added Sheep.cs

---
+++ FarmRecords/Sheep.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarmRecords
+{
+    class Sheep
+    {
+        public int SheepId { get; set; }
+        public string HerdNumber { get; set; }
+        public string TagNumber { get; set; }
+        public string Breed { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public DateTime DateOfPurchase { get; set; }
+        public string PurchaseDocumentNumber { get; set; }
+        public char Sex { get; set; }
+        public string Status { get; set; }
+        public string Type { get; set; }
+    }
+}
