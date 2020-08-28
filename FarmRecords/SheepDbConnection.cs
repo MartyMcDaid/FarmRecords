15b9e2b7fa378312a09ec40d7519b056e45b62d0
marty.mcdaid@gmail.com <marty.mcdaid@gmail.com>
2020-08-28 09:13:46 +0100

    Added Database connection class

---
+++ FarmRecords/SheepDbConnection.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarmRecords
+{
+    internal class SheepDbConnection
+    {
+        internal SheepDbConnection()
+        {
+
+        }
+
+        public SqlConnection GetConnection()
+        {
+            String connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\SheepDb.mdf;Integrated Security=True";
+            SqlConnection connection = new SqlConnection(connectionString);
+            return connection;
+        }
+    }
+}
